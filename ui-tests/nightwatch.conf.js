50121482ce6ed22c1f2d594764977c8492f4c6b8
Sara Ramadan <sararamadan20005@gmail.com>
2025-07-06 02:54:11 +0300

    update

--- ui-tests/nightwatch.conf.js
+++ ui-tests/nightwatch.conf.js
@@ -1,10 +1,18 @@
+const path = require('path');
+const isWindows = process.platform === 'win32';
+
+const chromedriverPath = isWindows
+  ? path.resolve(__dirname, 'node_modules/chromedriver/lib/chromedriver/chromedriver.exe')
+  : '/usr/local/bin/chromedriver';  // لو تستخدم لينوكس أو ماك، عدل المسار حسب مكان chromedriver
+
 module.exports = {
   src_folders: ['tests'],
   page_objects_path: ['page-objects'],
+  output_folder: 'tests_output',
 
   webdriver: {
     start_process: true,
-    server_path: '/usr/local/bin/chromedriver',
+    server_path: chromedriverPath,
     port: 9515,
     cli_args: ['--verbose']
   },
@@ -17,12 +25,12 @@ module.exports = {
         'goog:chromeOptions': {
           args: [
             '--headless=new',
+            '--disable-gpu',
             '--no-sandbox',
             '--disable-dev-shm-usage',
             '--window-size=1280,800'
           ]
-        },
-        acceptInsecureCerts: true
+        }
       },
       screenshots: {
         enabled: true,
