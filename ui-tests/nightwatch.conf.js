8c71e258d4b4815ef9d05f5942c2890261abb1d2
Sara Ramadan <sararamadan20005@gmail.com>
2025-07-06 23:50:21 +0300

    update

--- ui-tests/nightwatch.conf.js
+++ ui-tests/nightwatch.conf.js
@@ -1,36 +1,65 @@
+const seleniumServer = require('selenium-server');
+const chromedriver = require('chromedriver');
+const geckodriver = require('geckodriver');
+const PKG = require('./package.json');
+
 module.exports = {
   src_folders: ['tests'],
-  page_objects_path: ['page-objects'],
-  output_folder: 'tests_output',
+  output_folder: './reports',
+  page_objects_path: './page-objects',
+  globals_path: './globals.js',
 
-  webdriver: {
+  selenium: {
     start_process: true,
-    server_path: 'chromedriver',  // ✅ Use built-in Chromedriver
-    port: 9515,
+    server_path: './node_modules/selenium-server-standalone-jar/jar/selenium-server-standalone-3.9.1.jar',
+    host: '127.0.0.1',
+    port: 4445,
+    cli_args: {
+      'webdriver.chrome.driver': './node_modules/.bin/chromedriver',
+      'webdriver.gecko.driver': './node_modules/.bin/geckodriver'
+    }
   },
 
   test_settings: {
     default: {
       launch_url: 'http://automationpractice.multiformis.com',
-      desiredCapabilities: {
-        browserName: 'chrome',
-        'goog:chromeOptions': {
-          w3c: true,
-          args: [
-            '--headless=new',
-            '--no-sandbox',
-            '--disable-gpu',
-            '--disable-dev-shm-usage',
-            '--window-size=1280,800'
-          ]
-        }
-      },
+      selenium_host: 'localhost',
+      selenium_port: 4445,
       screenshots: {
         enabled: true,
         path: 'screenshots',
         on_failure: true,
         on_error: true
+      },
+      desiredCapabilities: {
+        browserName: 'chrome',
+        chromeOptions: {
+          args: [
+            'headless',
+            'no-sandbox',
+            'disable-dev-shm-usage',
+            'window-size=1280,800'
+          ]
+        }
+      }
+    },
+
+    circleci: {
+      output_folder: process.env.CIRCLE_TEST_REPORTS || './reports'
+    },
+
+    chrome: {
+      desiredCapabilities: {
+        browserName: 'chrome'
+      }
+    },
+
+    firefox: {
+      desiredCapabilities: {
+        browserName: 'firefox'
       }
     }
   }
 };
+
+module.exports.TEST_RESULTS_PATH = './reports';
