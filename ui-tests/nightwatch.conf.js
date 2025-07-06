a5905120e7448f753c5958937d87ccedeab72bb1
Sara Ramadan <sararamadan20005@gmail.com>
2025-07-07 00:12:17 +0300

    update

--- ui-tests/nightwatch.conf.js
+++ ui-tests/nightwatch.conf.js
@@ -1,50 +1,54 @@
-const seleniumServer = require('selenium-server');
-const chromedriver = require('chromedriver');
-const geckodriver = require('geckodriver');
-
 module.exports = {
-  src_folders: ['tests'],
-  page_objects_path: ['page-objects'],
-  output_folder: 'tests_output',
+  src_folders: ["tests"],
+  page_objects_path: ["page-objects"],
+  output_folder: "tests_output",
+  custom_commands_path: "",
+  custom_assertions_path: "",
 
-  selenium: {
+  // WebDriver Settings
+  webdriver: {
     start_process: true,
-    server_path: seleniumServer.path,
-    port: 4444,
-    cli_args: {
-      'webdriver.chrome.driver': chromedriver.path,
-      'webdriver.gecko.driver': geckodriver.path
-    }
+    port: 9515,
+    server_path: "", // Let Nightwatch auto-detect Chromedriver
+    cli_args: ["--verbose"],
   },
 
+  // Test Settings
   test_settings: {
     default: {
-      launch_url: 'http://automationpractice.multiformis.com',
-      selenium_host: '127.0.0.1',
-      selenium_port: 4444,
+      launch_url: "http://automationpractice.multiformis.com",
       desiredCapabilities: {
-        browserName: 'chrome',
-        javascriptEnabled: true,
-        acceptSslCerts: true,
-        chromeOptions: {
-          args: ['--headless', '--no-sandbox', '--disable-gpu']
-        }
-      }
+        browserName: "chrome",
+        "goog:chromeOptions": {
+          args: [
+            "--headless=new",
+            "--disable-gpu",
+            "--no-sandbox",
+            "--disable-dev-shm-usage",
+            "--window-size=1280,800",
+          ],
+        },
+      },
+      screenshots: {
+        enabled: true,
+        path: "screenshots",
+        on_failure: true,
+        on_error: true,
+      },
+    },
+
+    chrome_headless: {
+      extends: "default", // Inherits from default settings
     },
 
     chrome: {
+      extends: "default",
       desiredCapabilities: {
-        browserName: 'chrome',
-        chromeOptions: {
-          args: ['--headless', '--no-sandbox', '--disable-gpu']
-        }
-      }
+        browserName: "chrome",
+        "goog:chromeOptions": {
+          args: ["--disable-gpu", "--no-sandbox", "--window-size=1280,800"],
+        },
+      },
     },
-
-    firefox: {
-      desiredCapabilities: {
-        browserName: 'firefox'
-      }
-    }
-  }
+  },
 };
