61bab6f2ac8b978aaeb5823a42fa767715db2312
Sara Ramadan <sararamadan20005@gmail.com>
2025-07-06 23:35:22 +0300

    update

--- ui-tests/nightwatch.conf.js
+++ ui-tests/nightwatch.conf.js
@@ -1,45 +1,55 @@
+const seleniumServer = require('selenium-server');
+const chromedriver = require('chromedriver');
+const PKG = require('./package.json');
+
+const TEST_RESULTS_PATH = './test-results/' + PKG.version + '/';
+
 module.exports = {
   src_folders: ['tests'],
   page_objects_path: ['page-objects'],
-  output_folder: 'tests_output',
+  output_folder: TEST_RESULTS_PATH,
 
-  webdriver: {
+  selenium: {
     start_process: true,
-    port: 9515,
-    server_path: process.env.CI ? '/usr/bin/chromedriver' : '',   // Correct for CircleCI + Local
-    cli_args: []
+    server_path: seleniumServer.path,
+    host: '127.0.0.1',
+    port: 4444,
+    cli_args: {
+      'webdriver.chrome.driver': chromedriver.path
+    }
   },
 
   test_settings: {
-    chrome_headless: {
-      launch_url: 'http://automationpractice.multiformis.com',
-      desiredCapabilities: {
-        browserName: 'chrome',
-        'goog:chromeOptions': {
-          args: [
-            '--headless=new',
-            '--disable-gpu',
-            '--no-sandbox',
-            '--disable-dev-shm-usage',
-            '--window-size=1280,800'
-          ]
-        }
-      },
+    default: {
       screenshots: {
         enabled: true,
-        path: 'screenshots',
+        path: TEST_RESULTS_PATH,
         on_failure: true,
         on_error: true
+      },
+      globals: {
+        waitForConditionTimeout: 5000
+      },
+      desiredCapabilities: {
+        browserName: 'chrome',
+        javascriptEnabled: true,
+        acceptSslCerts: true,
+        chromeOptions: {
+          args: ['headless', 'disable-gpu']
+        }
       }
     },
     chrome: {
-      launch_url: 'http://automationpractice.multiformis.com',
       desiredCapabilities: {
         browserName: 'chrome',
-        'goog:chromeOptions': {
-          args: ['--window-size=1280,800']
+        javascriptEnabled: true,
+        acceptSslCerts: true,
+        chromeOptions: {
+          args: ['headless']
         }
       }
     }
   }
 };
+
+module.exports.TEST_RESULTS_PATH = TEST_RESULTS_PATH;
