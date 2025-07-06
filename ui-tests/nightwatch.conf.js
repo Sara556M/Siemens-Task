b3cd7ca07459d39f2a0b5d335414a37e167b7cfb
Sara Ramadan <sararamadan20005@gmail.com>
2025-07-07 00:23:03 +0300

    Update CI configuration

--- ui-tests/nightwatch.conf.js
+++ ui-tests/nightwatch.conf.js
@@ -1,54 +1,50 @@
 module.exports = {
-  src_folders: ["tests"],
-  page_objects_path: ["page-objects"],
-  output_folder: "tests_output",
-  custom_commands_path: "",
-  custom_assertions_path: "",
+  src_folders: ['tests'],
+  page_objects_path: ['page-objects'],
+  output_folder: 'tests_output',
+  custom_commands_path: '',
+  custom_assertions_path: '',
 
-  // WebDriver Settings
   webdriver: {
     start_process: true,
     port: 9515,
-    server_path: "", // Let Nightwatch auto-detect Chromedriver
-    cli_args: ["--verbose"],
+    server_path: '',
+    cli_args: ['--verbose']
   },
 
-  // Test Settings
   test_settings: {
     default: {
-      launch_url: "http://automationpractice.multiformis.com",
+      launch_url: 'http://automationpractice.multiformis.com',
       desiredCapabilities: {
-        browserName: "chrome",
-        "goog:chromeOptions": {
+        browserName: 'chrome',
+        'goog:chromeOptions': {
           args: [
-            "--headless=new",
-            "--disable-gpu",
-            "--no-sandbox",
-            "--disable-dev-shm-usage",
-            "--window-size=1280,800",
-          ],
-        },
+            '--headless=new',
+            '--disable-gpu',
+            '--no-sandbox',
+            '--disable-dev-shm-usage',
+            '--window-size=1280,800'
+          ]
+        }
       },
       screenshots: {
         enabled: true,
-        path: "screenshots",
+        path: 'screenshots',
         on_failure: true,
-        on_error: true,
-      },
+        on_error: true
+      }
     },
-
     chrome_headless: {
-      extends: "default", // Inherits from default settings
+      extends: 'default'
     },
-
     chrome: {
-      extends: "default",
+      extends: 'default',
       desiredCapabilities: {
-        browserName: "chrome",
-        "goog:chromeOptions": {
-          args: ["--disable-gpu", "--no-sandbox", "--window-size=1280,800"],
-        },
-      },
-    },
-  },
+        browserName: 'chrome',
+        'goog:chromeOptions': {
+          args: ['--disable-gpu', '--no-sandbox', '--window-size=1280,800']
+        }
+      }
+    }
+  }
 };
