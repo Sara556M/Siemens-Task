9be6ff227c0718cb889406e24e220dba9162e380
Sara Ramadan <sararamadan20005@gmail.com>
2025-07-07 01:19:06 +0300

    update

--- ui-tests/nightwatch.conf.js
+++ ui-tests/nightwatch.conf.js
@@ -2,15 +2,34 @@ module.exports = {
   src_folders: ['tests'],
   page_objects_path: ['page-objects'],
   output_folder: 'tests_output',
+  custom_commands_path: '',
+  custom_assertions_path: '',
+  globals_path: '',
+
   webdriver: {
     start_process: true,
     port: 9515,
     server_path: '', // Let CircleCI handle ChromeDriver
-    cli_args: ['--verbose']
+    log_path: 'logs',
+    cli_args: [
+      '--verbose'
+    ]
   },
+
   test_settings: {
-    chrome_headless: {
+    default: {
       launch_url: 'http://automationpractice.multiformis.com',
+      selenium_host: 'localhost',
+      silent: true,
+      screenshots: {
+        enabled: true,
+        path: 'screenshots',
+        on_failure: true,
+        on_error: true
+      }
+    },
+
+    chrome_headless: {
       desiredCapabilities: {
         browserName: 'chrome',
         'goog:chromeOptions': {
@@ -21,14 +40,31 @@ module.exports = {
             '--disable-dev-shm-usage',
             '--window-size=1280,800'
           ],
-          binary: '/usr/bin/google-chrome' // Explicit Chrome path
+          binary: '/usr/bin/google-chrome' // Explicit path for CircleCI
         }
-      },
-      screenshots: {
-        enabled: true,
-        path: 'screenshots',
-        on_failure: true
       }
+    },
+
+    chrome: {
+      desiredCapabilities: {
+        browserName: 'chrome',
+        'goog:chromeOptions': {
+          args: [
+            '--disable-gpu',
+            '--no-sandbox',
+            '--window-size=1280,800'
+          ],
+          binary: '/usr/bin/google-chrome'
+        }
+      }
+    }
+  },
+
+  test_runner: {
+    type: 'mocha',
+    options: {
+      ui: 'bdd',
+      reporter: 'list'
     }
   }
 };
