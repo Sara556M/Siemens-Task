8f2eb93cb8e85c3e7d4e1bc2ded553593500b5c5
Sara Ramadan <sararamadan20005@gmail.com>
2025-07-07 01:43:25 +0300

    update

--- ui-tests/nightwatch.conf.js
+++ ui-tests/nightwatch.conf.js
@@ -6,7 +6,7 @@ module.exports = {
   webdriver: {
     start_process: true,
     port: 9515,
-    server_path: '', // Let CircleCI handle ChromeDriver
+    server_path: '', // Uses system ChromeDriver
     cli_args: ['--verbose']
   },
 
