1e67598c847c354900d899e43c0f25774604b954
Sara Ramadan <sararamadan20005@gmail.com>
2025-07-06 03:02:56 +0300

    Update nightwatch.conf.js

--- ui-tests/nightwatch.conf.js
+++ ui-tests/nightwatch.conf.js
@@ -1,10 +1,3 @@
-const path = require('path');
-const isWindows = process.platform === 'win32';
-
-const chromedriverPath = isWindows
-  ? path.resolve(__dirname, 'node_modules/chromedriver/lib/chromedriver/chromedriver.exe')
-  : '/usr/local/bin/chromedriver';  // لو تستخدم لينوكس أو ماك، عدل المسار حسب مكان chromedriver
-
 module.exports = {
   src_folders: ['tests'],
   page_objects_path: ['page-objects'],
@@ -12,7 +5,6 @@ module.exports = {
 
   webdriver: {
     start_process: true,
-    server_path: chromedriverPath,
     port: 9515,
     cli_args: ['--verbose']
   },
