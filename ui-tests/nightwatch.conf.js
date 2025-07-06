a9cb17572b4f9e278e6ecd39e1209154bdea00f3
Sara Ramadan <sararamadan20005@gmail.com>
2025-07-07 00:01:16 +0300

    update

--- ui-tests/nightwatch.conf.js
+++ ui-tests/nightwatch.conf.js
@@ -1,56 +1,43 @@
 const seleniumServer = require('selenium-server');
 const chromedriver = require('chromedriver');
 const geckodriver = require('geckodriver');
-const PKG = require('./package.json');
 
 module.exports = {
   src_folders: ['tests'],
-  output_folder: './reports',
-  page_objects_path: './page-objects',
-  globals_path: './globals.js',
+  page_objects_path: ['page-objects'],
+  output_folder: 'tests_output',
 
   selenium: {
     start_process: true,
-    server_path: './node_modules/selenium-server-standalone-jar/jar/selenium-server-standalone-3.9.1.jar',
-    host: '127.0.0.1',
-    port: 4445,
+    server_path: seleniumServer.path,
+    port: 4444,
     cli_args: {
-      'webdriver.chrome.driver': './node_modules/.bin/chromedriver',
-      'webdriver.gecko.driver': './node_modules/.bin/geckodriver'
+      'webdriver.chrome.driver': chromedriver.path,
+      'webdriver.gecko.driver': geckodriver.path
     }
   },
 
   test_settings: {
     default: {
       launch_url: 'http://automationpractice.multiformis.com',
-      selenium_host: 'localhost',
-      selenium_port: 4445,
-      screenshots: {
-        enabled: true,
-        path: 'screenshots',
-        on_failure: true,
-        on_error: true
-      },
+      selenium_host: '127.0.0.1',
+      selenium_port: 4444,
       desiredCapabilities: {
         browserName: 'chrome',
+        javascriptEnabled: true,
+        acceptSslCerts: true,
         chromeOptions: {
-          args: [
-            'headless',
-            'no-sandbox',
-            'disable-dev-shm-usage',
-            'window-size=1280,800'
-          ]
+          args: ['--headless', '--no-sandbox', '--disable-gpu']
         }
       }
     },
 
-    circleci: {
-      output_folder: process.env.CIRCLE_TEST_REPORTS || './reports'
-    },
-
     chrome: {
       desiredCapabilities: {
-        browserName: 'chrome'
+        browserName: 'chrome',
+        chromeOptions: {
+          args: ['--headless', '--no-sandbox', '--disable-gpu']
+        }
       }
     },
 
@@ -61,5 +48,3 @@ module.exports = {
     }
   }
 };
-
-module.exports.TEST_RESULTS_PATH = './reports';
